9c0e8cb4d4f57643793a1daad04a40084a38532b
Stamp <stampz74@gmail.com>
2017-08-02 17:54:53 +0700

    in case of testing failed

--- src/api/__tests__/github.test.js
+++ src/api/__tests__/github.test.js
@@ -10,7 +10,7 @@ const github = require('../github')
 // A simple example test
 describe('#getUser() using Promises', () => {
   it('should load user data', async () => {
-    const data = await github.getUser('vnglst')
+    const data = await github.getUser('vnglsts')
     expect(data).toBeDefined()
     expect(data.entity.name).toEqual('Koen van Gilst')
   })
